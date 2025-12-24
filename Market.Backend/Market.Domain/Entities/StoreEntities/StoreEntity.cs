e5b14f978650529a1fa0c20f82f5860593564c1e
Kerim Omanović <kerim.omanovic@edu.fit.ba>
2025-12-24 22:27:53 +0100

    #6 S1-T1 Added StoreEntites

---
+++ Market.Backend/Market.Domain/Entities/StoreEntities/StoreEntity.cs
@@ -0,0 +1,19 @@
+﻿using Market.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Domain.Entities.StoreEntities
+{
+    public class StoreEntity : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Contact { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+        public int CityId { get; set; }
+        //public CityEntity? City { get; set; }
+    }
+}
