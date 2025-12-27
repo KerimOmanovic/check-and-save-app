c63b0466fe1c13492c6a8716d6a1850ef8975ee4
Amra <amra.karalic@edu.fit.ba>
2025-12-27 19:54:37 +0100

    #14 Completed Customer Entities

---
+++ Market.Backend/Market.Domain/Entities/Identity/CustomerEntity.cs
@@ -0,0 +1,17 @@
+﻿using Market.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Domain.Entities.Identity
+{
+    public class CustomerEntity : BaseEntity
+    {
+        public int MarketUserEntityId { get; set; }
+        public MarketUserEntity?MarketUserEntity { get; set; }
+        public int Points {  get; set; }
+        public int AvatarLevel { get; set; }
+    }
+}
---
+++ Market.Backend/Market.Domain/Entities/Identity/SecurityQuestionEntity.cs
@@ -0,0 +1,18 @@
+﻿using Market.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Domain.Entities.Identity
+{
+    public class SecurityQuestionEntity : BaseEntity
+    {
+        public int MarketUserEntityId { get; set; }
+
+        public MarketUserEntity? MarketUserEntity { get; set; }
+        public string Question { get; set; } = null!;
+        public string Answer { get; set; } = null!;
+    }
+}
