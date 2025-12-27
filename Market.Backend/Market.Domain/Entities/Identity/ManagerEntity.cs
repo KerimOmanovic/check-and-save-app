52e3de66e3fcf3ae9f7099250bf0bf58bfeb4f78
Amra <amra.karalic@edu.fit.ba>
2025-12-27 19:54:37 +0100

    #14 Completed Customer Entities

---
+++ Market.Backend/Market.Domain/Entities/Identity/ManagerEntity.cs
@@ -0,0 +1,20 @@
+﻿using Market.Domain.Common;
+using Market.Domain.Entities.StoreEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Domain.Entities.Identity
+{
+    public class ManagerEntity : BaseEntity
+    {
+        public int MarketUserEntityId { get; set; }
+        public MarketUserEntity?MarketUserEntity { get; set; }
+
+        public int StoreEntityId { get; set; }
+        public StoreEntity?StoreEntity { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
