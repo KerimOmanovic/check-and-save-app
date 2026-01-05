a1246c44d8f38a9c748b7d15214c45e52301efcf
Kerim Omanović <kerim.omanovic@edu.fit.ba>
2026-01-05 21:35:30 +0100

    #9

--- Market.Backend/Market.Application/Modules/Identity/SecurityQuestion/Command/Delete/DeleteSecurityQuestionCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Market.Application.Modules.Identity.SecurityQuestions.Commands.Delete;
-
-public sealed class DeleteSecurityQuestionCommand : IRequest<Unit>
-{
-    public int Id { get; set; }
-}
