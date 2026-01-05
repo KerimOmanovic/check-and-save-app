f4849163c0a638fa6a5202ed99ccb9544fafbdec
Kerim Omanović <kerim.omanovic@edu.fit.ba>
2026-01-05 21:22:04 +0100

    #9

---
+++ Market.Backend/Market.Infrastructure/Database/Configurations/Identity/PublicUserEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace Market.Infrastructure.Database.Configurations.Identity;
+
+public sealed class PublicUserEntityConfiguration
+    : IEntityTypeConfiguration<PublicUserEntity>
+{
+    public void Configure(EntityTypeBuilder<PublicUserEntity> b)
+    {
+        b.ToTable("PublicUsers");
+
+        b.HasKey(x => x.Id);
+
+        b.HasOne(x => x.MarketUserEntity)
+            .WithOne(x => x.PublicUserEntity)
+            .HasForeignKey<PublicUserEntity>(x => x.MarketUserEntityId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        b.Property(x => x.Points)
+            .HasDefaultValue(PublicUserEntity.Constraints.MinPoints);
+
+        b.Property(x => x.AvatarLevel)
+            .HasDefaultValue(PublicUserEntity.Constraints.MinAvatarLevel);
+    }
+}
