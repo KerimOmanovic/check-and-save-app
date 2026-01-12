fd45186b8310043d9bd204027a4ee25ce1bd7428
Kerim Omanović <kerim.omanovic@edu.fit.ba>
2026-01-12 21:49:19 +0100

    #22 Created SaleStatistic and Report entitites

---
+++ Market.Backend/Market.Domain/Entities/Analytics/ReportEntity.cs
@@ -0,0 +1,26 @@
+﻿using Market.Domain.Common;
+using Market.Domain.Entities.Identity;
+
+namespace Market.Domain.Entities.Analytics
+{
+    public class ReportEntity : BaseEntity
+    {
+        public int MarketUserEntityId { get; set; }
+
+        public string ReportType { get; set; } = default!;
+        public string? Description { get; set; }
+        public DateTime ReportDate { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public MarketUserEntity MarketUserEntity { get; set; } = default!;
+
+        public static class Constraints
+        {
+            public const int ReportTypeMaxLength = 100;
+            public const int DescriptionMaxLength = 2000;
+        }
+    }
+}
---
+++ Market.Backend/Market.Domain/Entities/Analytics/SalesStatisticEntity.cs
@@ -0,0 +1,23 @@
+﻿using Market.Domain.Common;
+using Market.Domain.Entities.Identity;
+using Market.Domain.Entities.ProductEntities;
+
+namespace Market.Domain.Entities.Analytics
+{
+    public class SalesStatisticEntity : BaseEntity
+    {
+        public int ManagerEntityId { get; set; }
+        public int ProductEntityId { get; set; }
+
+        public int ViewsCount { get; set; }
+        public int SalesCount { get; set; }
+        public DateTime Date { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public ManagerEntity? ManagerEntity { get; set; } = default!;
+        public ProductEntity? ProductEntity { get; set; } = default!;
+    }
+}
