484aeb87c800dc64fb29a816bbf05d8b6cf28bdd
Amra <amra.karalic@edu.fit.ba>
2026-01-12 21:45:00 +0100

    #17 Create Notification & NotificationType entities

---
+++ Market.Backend/Market.Domain/Entities/NotificationEntities/NotificationEntity.cs
@@ -0,0 +1,25 @@
+﻿using Market.Domain.Common;
+using Market.Domain.Entities.Identity;
+namespace Market.Domain.Entities.NotificationEntities
+{
+    public sealed class NotificationEntity : BaseEntity
+    {
+
+        public int MarketUserEntityId { get; set; }
+        public MarketUserEntity? MarketUserEntity { get; set; }
+
+
+        public int NotificationTypeEntityId { get; set; }
+        public NotificationTypeEntity? NotificationType { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public bool IsRead { get; set; }
+
+        public static class Constraints
+        {
+            public const int TitleMaxLength = 200;
+            public const int MessageMaxLength = 2000;
+        }
+    }
+}
