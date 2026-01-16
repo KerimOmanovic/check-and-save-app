b0b6a86f306f23f0ae2279cba0162916de0325cf
Amra <amra.karalic@edu.fit.ba>
2026-01-16 21:24:22 +0100

    #20 Create CRUD Notifications

---
+++ Market.Backend/Market.Application/Modules/Notifications/Notification/Commands/Update/UpdateNotificationCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿namespace Market.Application.Modules.Notifications.Notification.Commands.Update
+{
+    public sealed class UpdateNotificationCommandHandler(IAppDbContext ctx) : IRequestHandler<UpdateNotificationCommand, Unit>
+    {
+        public async Task<Unit> Handle(UpdateNotificationCommand request, CancellationToken ct)
+        {
+            var entity = await ctx.Notifications
+                .Where(x => x.Id == request.Id)
+                .FirstOrDefaultAsync(ct);
+
+            if (entity is null)
+                throw new MarketNotFoundException($"Notifikacija (ID={request.Id}) nije pronađena.");
+
+ 
+            var exists = await ctx.Notifications
+                .AnyAsync(x =>
+                    x.Id != request.Id &&
+                    x.MarketUserEntityId == entity.MarketUserEntityId &&
+                    x.Title.ToLower() == request.Title.ToLower(),
+                    ct);
+
+            if (exists)
+            {
+                throw new MarketConflictException("Notification with the same title already exists for this user.");
+            }
+
+            entity.Title = request.Title.Trim();
+            entity.Message = request.Message.Trim();
+            entity.IsRead = request.IsRead;
+
+            await ctx.SaveChangesAsync(ct);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ Market.Backend/Market.Application/Modules/Notifications/Notification/Queries/GetById/GetNotificationByIdQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿namespace Market.Application.Modules.Notifications.Notification.Queries.GetById
+{
+    public sealed class GetNotificationByIdQueryValidator : AbstractValidator<GetNotificationByIdQuery>
+    {
+        public GetNotificationByIdQueryValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0)
+                .WithMessage("Id must be a positive value.");
+        }
+    }
+}
