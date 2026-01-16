9f45ebe73614bcee1a8090cd119df22523cbcdee
Amra <amra.karalic@edu.fit.ba>
2026-01-16 21:24:22 +0100

    #20 Create CRUD Notifications

---
+++ Market.Backend/Market.Application/Modules/Notifications/Notification/Commands/Create/CreateNotificationCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace Market.Application.Modules.Notifications.Notification.Commands.Create
+{
+    public sealed class CreateNotificationCommand : IRequest<int>
+    {
+        public int MarketUserEntityId { get; set; }
+        public int NotificationTypeEntityId { get; set; }
+
+        public required string Title { get; set; }
+        public required string Message { get; set; }
+        public bool IsRead { get; set; } = false;
+    }
+}
