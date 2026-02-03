9d6c720f75db6b09df449b3eb563e9e3b506a706
Amra <amra.karalic@edu.fit.ba>
2026-02-03 21:16:53 +0100

    #55 done

--- Market.Frontend/rs1-frontend-2025-26/src/app/modules/client/client-routing-module.ts
+++ Market.Frontend/rs1-frontend-2025-26/src/app/modules/client/client-routing-module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './client-pages/home-page/home.component';
+import { FavoritesComponent } from './client-pages/favorites-page/favorites.component';
+import { MapComponent } from './client-pages/map-page/map.component';
+import { ProfileComponent } from './client-pages/profile-page/profile.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: 'map', component: MapComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
